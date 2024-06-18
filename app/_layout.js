2e5b777cad0c3c8481aec99675c8657db44cc9d4
csark0812 <chris@getgreco.app>
2024-06-18 10:32:33 -0700

    first commit

---
+++ app/_layout.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { View, FlatList, RefreshControl } from 'react-native';
+
+const TestRefreshControl = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => setRefreshing(false), 2000);
+  };
+
+  return (
+    <View style={{ flex: 1, paddingTop: 150 }}>
+      <FlatList
+        data={[{ key: '1' }, { key: '2' }, { key: '3' }]}
+        renderItem={({ item }) => <View style={{ height: 100, backgroundColor: 'lightgray', margin: 10 }} />}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            tintColor="blue"  // Change the tintColor here
+            progressViewOffset={50}
+          />
+        }
+      />
+    </View>
+  );
+};
+
+export default TestRefreshControl;
